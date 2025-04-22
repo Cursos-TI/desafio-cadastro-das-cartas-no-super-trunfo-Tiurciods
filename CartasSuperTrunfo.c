4ed23fb8f1f4c884118504cd9c86bf841b924d59
Marcelo Tiburcio <marcelotrabalho-1999@hotmail.com>
2025-04-22 11:50:50 +0000

    Desenvolvendo o Códigp

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,9 +4,36 @@
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
 
 int main() {
+
+    char letra1, letra2; // Uma letra de 'A' a 'H' (representando um dos dois estados) 
+    char codigo1[3], codigo2[3]; // A letra do estado seguida de um número de 01 a 04 (ex: A01, B03)
+    char cidade1[20], cidade2[20]; // O nome da cidade
+    int população1, população2; // O número de habitantes da cidade
+    float area1, area2; // A área da cidade em quilômetros quadrados
+    float pib1, pib2; // O Produto Interno Bruto da cidade
+    int turistico1, turistico2; // A quantidade de pontos turísticos na cidade.
+
+    //printf(''texto com formatação'', variavel1, variavel2, ...);
+    //scanf(''formato'', &variavel);
+
+    printf("Cadastro da Carta 01:\n");
+    printf("Digite a Letra qe representa o Estado: ");
+    scanf("%s", letra1);
+
+    
+
+
+
+
+
+
+
+
+
+
+
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
@@ -20,3 +47,16 @@ int main() {
 
     return 0;
 }
+/*
+%d: Imprime um inteiro no formato decimal.
+ 
+%i: Equivalente a %d.
+ 
+%f: Imprime um número de ponto flutuante no formato padrão.
+ 
+%e: Imprime um número de ponto flutuante na notação científica.
+ 
+%c: Imprime um único caractere.
+ 
+%s: Imprime uma cadeia (string) de caracteres.
+*/
